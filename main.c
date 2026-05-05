e370f06b095a350f8bede3e660f859927c5d54e9
SandorMore <msaso0725@gmail.com>
2026-05-05 19:32:42 +0200

    Added stuff

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(int argc, char** argv){
-	printf("Started");
+	for(size_t i = 1; i <= argc; ++i){
+		printf("%s\n", argv[i]);
+	}
 	return 0;
-;}
+}
